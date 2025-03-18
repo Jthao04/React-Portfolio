2dbc657789e13c62565390d9aaedbbce65178fcb
Your Name <you@example.com>
2025-03-17 19:00:39 -0500

    navbar

--- React Portfolio/src/components/Navigation.jsx
+++ React Portfolio/src/components/Navigation.jsx
@@ -1,12 +1,10 @@
 import { Link, useLocation } from 'react-router-dom';
 
-// Here we are using object destructuring assignment to pluck off our variables from the props object
-// We assign them to their own variable names
 function Navigation() {
     const currentPage = useLocation().pathname;
 
     return (
-        <nav className="navbar navbar-expand-lg bg-secondary pb-0">
+        <nav className="navbar navbar-expand-lg bg-secondary pb-0 fixed-top">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">
                     <img src="https://img.icons8.com/hatch/64/mario.png" alt="Logo" width="30" height="30" className="d-inline-block align-text-top tertiary" />
@@ -16,8 +14,6 @@ function Navigation() {
                     <li className="nav-item">
                         <Link
                             to="/"
-                            // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-                            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
                             className={currentPage === '/' ? 'nav-link active bg-primary' : 'nav-link bg-dark'}
                         >
                             About
@@ -26,7 +22,6 @@ function Navigation() {
                     <li className="nav-item">
                         <Link
                             to="/Portfolio"
-                            // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                             className={currentPage === '/Portfolio' ? 'nav-link active bg-primary' : 'nav-link bg-dark'}
                         >
                             Portfolio
@@ -35,7 +30,6 @@ function Navigation() {
                     <li className="nav-item">
                         <Link
                             to="/Contact"
-                            // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                             className={currentPage === '/Contact' ? 'nav-link active bg-primary' : 'nav-link bg-dark'}
                         >
                             Contact
@@ -44,7 +38,6 @@ function Navigation() {
                     <li className="nav-item">
                         <Link
                             to="/Resume"
-                            // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
                             className={currentPage === '/Resume' ? 'nav-link active bg-primary' : 'nav-link bg-dark'}
                         >
                             Resume
